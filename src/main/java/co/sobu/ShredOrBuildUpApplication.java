608ed211ea6f9b72c2efb934b0f1f8636c3fd4b8
AServantDev <arnaud.github@gmail.com>
2018-11-12 15:02:28 +0100

    basic structure for springboot

---
+++ src/main/java/co/sobu/ShredOrBuildUpApplication.java
@@ -0,0 +1,12 @@
+package co.sobu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShredOrBuildUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShredOrBuildUpApplication.class, args);
+	}
+}
